7a6b48017b10e97d5943cab492e324d2fbb662e9
Abhix <abhinabasen2022@gmail.com>
2023-03-27 14:25:48 +0530

    Commit 3

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-music1;
-music2;
+music1 = null;
+music2 = null;
+leftX = 0;
+leftY = 0;
+rightX = 0;
+rightY = 0;
 
 /*Commit*/
 function preload(){
@@ -11,8 +15,23 @@ function setup(){
 canvas = createCanvas(600,400);
 video  = createCapture(VIDEO);
 video.hide();
+posenet = ml5.poseNet(video,modelLoaded);
+posenet.on('pose',gotPoses);
 }
 
 function draw(){
     image(video,0,0,600,400)
+}
+
+function gotPoses(results){
+    if (results.length > 0){
+        leftX = results[0].pose.leftWrist.x;
+        leftY = results[0].pose.leftWrist.y;
+        rightX = results[0].pose.rightWrist.x;
+        rightY = results[0].pose.rightWrist.y;
+    }
+}
+
+function modelLoaded(){
+    console.log("ml5 model loaded with versione - " + " " + ml5.version);
 }
