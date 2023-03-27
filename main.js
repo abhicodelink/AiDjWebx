422778810866eaecae97a08ca81cd83d85bff618
Abhix <abhinabasen2022@gmail.com>
2023-03-27 13:58:34 +0530

    Commit 2

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-/*Mommit*/
+music1;
+music2;
+
+/*Commit*/
+function preload(){
+    music1 = loadSound('music.mp3');
+    music2 = loadSound('music2.mp3');
+}
+
+function setup(){
+canvas = createCanvas(600,400);
+video  = createCapture(VIDEO);
+video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,400)
+}
